7b3a5c8e169cb9aba5749c458e1cf9fb79f444c5
prostraction <47314760+prostraction@users.noreply.github.com>
2024-06-28 15:24:01 +0300

    Finished all tasks, made CSS more close to the layout

--- script.js
+++ script.js
@@ -8,14 +8,13 @@ const getMediaUrl = (idInstance) => {
     return `https://${id}.media.green-api.com`;
 };
 
+const idInstanceInput = document.querySelector('#idInstanceInput');
+const apiTokenInstanceInput = document.querySelector('#apiTokenInstanceInput');
 const getIdAndApi = () => {
-    const idInstanceInput = document.querySelector('#idInstanceInput');
-    const apiTokenInstanceInput = document.querySelector('#apiTokenInstanceInput');
     return [idInstanceInput.value, apiTokenInstanceInput.value];
 };
 
 const answerText = document.querySelector('#answerText');
-
 const makeRequest = (uri, request) => {
     fetch(uri, request)
     .then(res => {return (res.ok) ? res.json() : Promise.reject(res);})
@@ -66,30 +65,59 @@ apiStateInstanceButton.addEventListener("click", function() {
     }
 });
 
+const phoneMessageInput = document.querySelector('#phoneMessageInput');
+const messageInput = document.querySelector('#messageInput')
 const sendMessageButton = document.querySelector('#sendMessageButton');
 sendMessageButton.addEventListener("click", function() {
     const [idInstance, apiTokenInstance] = getIdAndApi();
-    const phoneMessage = document.querySelector('#phoneMessageInput').value.replace(/\D/g, '');
-    const message = document.querySelector('#messageInput').value;
+    const phoneMessage = phoneMessageInput.value.replace(/\D/g, '');
+    const message = messageInput.value;
+
     if (inputDataIsOk(idInstance, apiTokenInstance)) {
         if (phoneMessage.length > 0 && message.length > 0) {
             const reqParams = {
                 chatId: `${phoneMessage}@c.us`,
                 message: message,
             };
-
             const uri = `${getApiUrl(idInstance)}/waInstance${idInstance}/sendMessage/${apiTokenInstance}`;
             makeRequest(uri, {
                 method: 'POST',
                 body: JSON.stringify(reqParams),
             });
         } else {
-            answerText.value = `Введите номер телефона и сообщение для отправки сообщения.`;
+            answerText.value = `Введите номер телефона и текст сообщения для его отправки.`;
         }
     }
 });
 
+const phoneFileInput = document.querySelector('#phoneFileInput');
+const messageFileInput = document.querySelector('#messageFileInput')
 const sendFileButton = document.querySelector('#sendFileButton');
 sendFileButton.addEventListener("click", function() {
-    console.log("sendFileButton clicked!");
+    const [idInstance, apiTokenInstance] = getIdAndApi();
+    const phoneMessage = phoneFileInput.value.replace(/\D/g, '');
+    const message = messageFileInput.value;
+    const messageFiltered = message.replace(/[^a-zA-Z0-9-_()%:./]+/g, '');
+    answerText.value = ``;
+    if (message !== messageFiltered) {
+        answerText.value =  `В ссылке могут быть использованы символы a-z, A-Z, 0-9, - (дефис), _ (нижнее подчеркивание). В ссылке должны отсутствовать спец символы ~$€%#£?! и пробелы.`;
+        return;
+    }
+
+    if (inputDataIsOk(idInstance, apiTokenInstance)) {
+        if (phoneMessage.length > 0 && message.length > 0) {
+            const reqParams = {
+                chatId: `${phoneMessage}@c.us`,
+                urlFile: messageFiltered,
+                fileName: messageFiltered.split("/").pop(),
+            };
+            const uri = `${getApiUrl(idInstance)}/waInstance${idInstance}/sendFileByUrl/${apiTokenInstance}`;
+            makeRequest(uri, {
+                method: 'POST',
+                body: JSON.stringify(reqParams),
+            });
+        } else {
+            answerText.value = `Введите номер телефона и Url для отправки сообщения.`;
+        }
+    }    
 });
